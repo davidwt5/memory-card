a894448b9353f19e11062109a2283aa91dd5743c
davidwt5 <davidwtan75@gmail.com>
2021-10-17 20:56:19 +1100

    reformated Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 
 function Card(props) {
-  return (<div className="card">
-    <img src={props.path} alt={props.name}/>
-    <p>{props.name}</p>
-  </div>);
+  return (
+    <div className="card">
+      <img src={props.path} alt={props.name} />
+      <p>{props.name}</p>
+    </div>
+  );
 }
 
 export default Card;
