32715ca612c7b5cb76c6688f29ef409a90281e5e
davidwt5 <davidwtan75@gmail.com>
2021-10-17 22:17:43 +1100

    organised components into heirarchies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import db from "./models/db.json";
 import Card from "./components/Card";
-import Scoreboard from "./components/Scoreboard";
+import Header from "./components/Header"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {db.cards.map((card) => (
-        <Card
-          key={card.fileName}
-          path={process.env.PUBLIC_URL + "cards/" + card.fileName}
-          name={card.name}
-        />
-      ))}
-      <Scoreboard score="0" highscore="10" />
+    <div className="app">
+      <Header />
+      <div className="cards">
+        {db.cards.map((card) => (
+          <Card
+            key={card.fileName}
+            path={process.env.PUBLIC_URL + "cards/" + card.fileName}
+            name={card.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
