11345f1b4e8fca1df7e0cb237670ec59397bd508
davidwt5 <davidwtan75@gmail.com>
2021-10-17 20:49:24 +1100

    moved static card files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <Card path={process.env.PUBLIC_URL + 'img/cards/hakos-baelz.webp'} name="Hakos Baelz"/>
+      <Card path={process.env.PUBLIC_URL + 'cards/hakos-baelz.webp'} name="Hakos Baelz"/>
     </div>
   );
 }
