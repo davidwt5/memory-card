9466899dbb3db5a89e472415b62bd4d255ce0191
davidwt5 <davidwtan75@gmail.com>
2021-10-18 19:43:32 +1100

    added click handler using id

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,15 @@ import "./Card.css";
 
 function Card(props) {
   return (
-    <div className="card">
+    <div className="card" data-id={props.id} onClick={clickHandler}>
       <img src={props.path} alt={props.name} />
       <p>{props.name}</p>
     </div>
   );
+
+  function clickHandler(e) {
+    props.resolveCard(e.currentTarget.dataset.id);
+  }
 }
 
 export default Card;
